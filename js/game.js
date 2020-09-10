ed50f3b8079f2eddccc9686bfeadb0a896ee888f
Saalim Khan <68114437+saalimkhan13@users.noreply.github.com>
2020-09-11 00:16:31 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -36,66 +36,83 @@ class Game{
         }
     
     play(){
-    form.hide();
+        
+                form.hide();
 
-    Player.getPlayerInfo();
+                Player.getPlayerInfo();
+                 image(back_img, 0, 0, 1000, 800);
+                 var x =100;
+                 var y=200;
+                 var index =0;
+                 drawSprites();
+                 for(var plr in allPlayers){
+                    
+                    
+                     index = index+1;
+                     x = 500-allPlayers[plr].distance;
+                     y=500;
+                     
+                     players[index -1].x = x;
+                     players[index - 1].y = y;
+                       
+                     if(index === player.index){
+                         
+                         fill("black");
+                         textSize(25);
+                         text(allPlayers[plr].name ,x-25,y+25);
 
-    if(allPlayers!== undefined){
-        image(back_img, 0, 0, 1000, 800);
-        var x =100;
-        var y=200;
-        var index = 0;
-        drawSprites();
-        for(var plr in allPlayers){
-            index = index+1;
-            x = 500-allPlayers[plr].distance;
-            y=500;
-            
-           players[index-1].x = x;
-           players[index-1].y = y;
-            
-            if(index === player.index){
+                         
+                     }
+                    
+                         textSize(25);
+                         fill("white");
+                         text("Player 1 :" +allPlayers.player1.score,50,50);
+                        text("Player 2 :" + allPlayers.player2.score, 50, 100);
+                 
+                 }
                 
-                fill("black");
-                textSize(25);
-                text(allPlayers[plr].name ,x-25,y+25);
-
-            }            
-        }
-    }
                 
-if (keyIsDown(RIGHT_ARROW) && player.index !== null) {
-player.distance -= 10
-player.update();
-}
-if (keyIsDown(LEFT_ARROW) && player.index !== null) {
-player.distance += 10
-player.update();
-}
+                 
 
-if (frameCount % 20 === 0) {
-    fruits = createSprite(random(100, 1000), 0, 100, 100);
-    fruits.velocityY = 6;
-    var rand = Math.round(random(1,5));
-    switch(rand){
-        case 1: fruits.addImage("fruit1",fruit1_img);
-        break;
-        case 2: fruits.addImage("fruit1", fruit2_img);
-        break;
-        case 3: fruits.addImage("fruit1", fruit3_img);
-        break;
-        case 4: fruits.addImage("fruit1", fruit4_img);
-        break;
-        case 5: fruits.addImage("fruit1", fruit5_img);
-        break;
-    }
-    fruitGroup.add(fruits);
+                if (keyIsDown(RIGHT_ARROW) && player.index !== null) {
+                    player.distance -= 10
+                    player.update();
+                }
+                if (keyIsDown(LEFT_ARROW) && player.index !== null) {
+                    player.distance += 10
+                    player.update();
+                }
+            
+                 if (frameCount % 20 === 0) {
+                     fruits = createSprite(random(100, 1000), 0, 100, 100);
+                     fruits.velocityY = 6;
+                     var rand = Math.round(random(1,5));
+                     switch(rand){
+                         case 1: fruits.addImage("fruit1",fruit1_img);
+                         break;
+                         case 2: fruits.addImage("fruit1", fruit2_img);
+                         break;
+                         case 3: fruits.addImage("fruit1", fruit3_img);
+                         break;
+                         case 4: fruits.addImage("fruit1", fruit4_img);
+                         break;
+                         case 5: fruits.addImage("fruit1", fruit5_img);
+                         break;
+                     }
+                     fruitGroup.add(fruits);
                      
                  }
                  
                   if (player.index !== null) {
-                     //fill code here, to destroy the objects.
-                    // fruits.destroy();
+                      for (var i = 0; i < fruitGroup.length; i++) {
+                          if (fruitGroup.get(i).isTouching(players)) {
+                              fruitGroup.get(i).destroy();
+                              player.score =player.score+1;
+                              player.update();
+                              
+                          }
+                          
+                      }
                   }
                 
 
